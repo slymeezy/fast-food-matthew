35db5e968e022f6e9ccc3cf3fd2ad48e49e0865f
adele matthew <m.adele@outlook.fr>
2019-10-11 21:31:34 +0200

    fast food matthew

---
+++ contact.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <form>
+      <h1>Contact:</h1>
+  <div class="col-sm-8">
+    <label for="exampleInputEmail1">Email*</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="john.doe@tld.com">
+   
+  
+  <div class="col-sm-4">
+    <label for="exampleInputName1">nom</label>
+    <input type="name" class="form-control" id="exampleInputName1" placeholder="john DOE">
+  </div>
+   <div class="col-sm-8">
+    <label for="exampleInputName1">Adresse</label>
+    <input type="name" class="form-control" id="exampleInputName1" placeholder="1234 Av. Albert Einstein">
+     <div class="col-sm-8">
+    <label for="exampleInputName1">Complement d'adresse</label>
+    <input type="name" class="form-control" id="exampleInputName1" placeholder="Appartement,studio,ou digicode">
+  </div>
+   <div class="col-sm-8">
+    <label for="exampleInputName1">ville </label>
+    <input type="name" class="form-control" id="exampleInputName1" >
+  <div >
+     <div class="col-sm-8">
+    <label for="exampleInputName1">Code postal</label>
+    <input type="name" class="form-control" id="exampleInputName1" >
+     <div class="col-sm-8">
+    <label for="exampleInputName1">Message</label>
+    <input type="name" class="form-control" id="exampleInputName1" >
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">S'inscrire à notre newletter</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Valider</button>
+</form>
+
+ <div class="col-sm-4"><h1>Commande:</h1> </div>
+    
+  <il>
+    Molestiae.
+  <br></il>
+  <il>
+    Rerum.
+  <br></il>
+  <il>
+    Quasi sit.
+  <br></il>
+  <il>
+    Consequatur quisquam
+  <br></il>
+  <il>
+    Ut.
+  <br></il>
+   
+  <button type="button" class="btn btn-success">Commander</button>
+  
+  
+  <button type="button" class="btn btn-danger">Supprimer la commande</button>
+
+ </div> 
+
+</body>
+</html>
---
+++ welcom.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  	
+  	
+  		
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-8"><h1>Welcome :</h1></div>
+    
+
+   <table class="table table-striped ">
+  <thead>
+    <tr>
+      <th scope="col"><img src="https://via.placeholder.com/200x200"alt="image"class="img-fluid img-thumbnail"></th>
+      <th scope="row"><img src="https://via.placeholder.com/200x200"alt="image"></th>
+      <th scope="row"><img src="https://via.placeholder.com/200x200"alt="image"></th>
+      <th scope="row"><img src="https://via.placeholder.com/200x200"alt="image"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="col">Consequatur qui</th>
+      <td scope="row">qui ut.</td>
+      <td scope="row">Molestiae.</td>
+      <td scope="row">Ut.</td>
+    </tr>
+    <tr>
+      <th scope="col"><button type="button" class="btn btn-primary">ajouter</button></th>
+      <td scope="row"><button type="button" class="btn btn-primary">ajouter</button></td>
+      <td scope="row"><button type="button" class="btn btn-primary">ajouter</button></td>
+      <td scope="row"><button type="button" class="btn btn-primary">ajouter</button></td>
+    </tr>
+    <thead>
+    <tr>
+      <th scope="col"><img src="https://via.placeholder.com/200x200"alt="image"></th>
+      <th scope="row"><img src="https://via.placeholder.com/200x200"alt="image"></th>
+      <th scope="row"><img src="https://via.placeholder.com/200x200"alt="image"></th>
+      <th scope="row"><img src="https://via.placeholder.com/200x200"alt="image"></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="col">illo consequunt</th>
+      <td scope="row">Rerum.</td>
+      <td scope="row">Assumenda dolor</td>
+      <td scope="row">Maiores</td>
+    </tr>
+     <tr>
+      <th scope="col"><button type="button" class="btn btn-primary">ajouter</button></th>
+      <td scope="row"><button type="button" class="btn btn-primary">ajouter</button></td>
+      <td scope="row"><button type="button" class="btn btn-primary">ajouter</button></td>
+      <td scope="row"><button type="button" class="btn btn-primary">ajouter</button></td>
+    </tr>
+  </tbody>
+
+</table>
+</div>
+ <div class="col-sm-4"><h1>Commande:</h1> </div>
+   	
+ 	<il>
+ 		Molestiae.
+ 	<br></il>
+ 	<il>
+ 		Rerum.
+ 	<br></il>
+ 	<il>
+ 		Quasi sit.
+ 	<br></il>
+ 	<il>
+ 		Consequatur quisquam
+ 	<br></il>
+ 	<il>
+ 		Ut.
+ 	<br></il>
+ 	 
+ 	<button type="button" class="btn btn-success">Commander</button>
+ 	
+ 	
+ 	<button type="button" class="btn btn-danger">Supprimer la commande</button>
+
+ </div> 
+
+ </body>
+</html>
+
+   
+ 
